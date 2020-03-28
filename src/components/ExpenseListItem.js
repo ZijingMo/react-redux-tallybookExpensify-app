4945c993dd9cbad65e43721a79c6f1274d885c6a
Zijing Mo <zijingmo888@gmail.com>
2020-03-28 18:01:35 -0500

    formatting the amount and create timestamp

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment'; // Timestamp format change
+import numeral from 'numeral'; // Expense format change
 
 // Destructuring the object to 'dispatch', 'id', 'description', 'amount', 'createAt'
 // The component is injected to the 'dispatch' function when we use connect() hoc to wrap it up
@@ -9,7 +11,11 @@ const ExpenseListItem = ({ id, description, amount, createAt }) => (
     <Link to={`/edit/${id}`}>
        <h2> { description } </h2> 
     </Link>
-    <p> {amount} - {createAt} </p>
+    <p> 
+      {numeral(amount / 100).format('$0,0.00')} 
+      - 
+      {moment(createAt).format('MMMM Do, YYYY (ddd)')} 
+    </p>
   </div>
 );
 
