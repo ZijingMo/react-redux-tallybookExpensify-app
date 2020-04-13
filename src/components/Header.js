8ef708a9321959f4d57b67705e299f77f38f6c40
Mo <zijingmo888@gmail.com>
2020-04-13 13:41:22 -0500

    CSS wrapper: login page and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth'; 
 
 // Destruct the object 'startLogout', which is a function  
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>A Tally Book: Expensify</h1>
-    <NavLink to="/dashboard" activeClassName="is-active" > Dashboard </NavLink>
-    <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
-    <button onClick = {startLogout}> Logout </button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__contentStyle">
+        <Link className="header__title" to="/dashboard">
+          <h1>Expensify - A Tally Book</h1>
+        </Link>
+        <button onClick = {startLogout}> Logout </button>
+      </div>
+    </div>
   </header>
 );
 
