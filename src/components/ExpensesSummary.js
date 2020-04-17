e1f5131d86c70345b62cf9d925ed12c07d2f7a83
Mo <zijingmo888@gmail.com>
2020-04-17 12:57:53 -0500

    CSS wrapper: summary and list filters

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import numeral from 'numeral';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import getVisibleExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 
@@ -10,8 +11,13 @@ export const ExpensesSummary = ( { expenseCount, expensesTotal } ) => {
   // Format change from number to money counting
   const formattedExpense = numeral(expensesTotal / 100).format('$0,0.00');
   return (
-    <div>
-      <h2>Emm.. You have {expenseCount} {SingularAndPluralChange}, and the total is {formattedExpense} </h2>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Hmm.. You have <span>{expenseCount}</span> {SingularAndPluralChange}, and the total is <span>{formattedExpense}</span> </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
