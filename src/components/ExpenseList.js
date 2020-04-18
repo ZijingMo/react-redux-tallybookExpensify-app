fe8586db5b6fc4b06166b58058c303c8cef36764
Mo <zijingmo888@gmail.com>
2020-04-18 16:15:48 -0500

    CSS wrapper: expenses list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,16 +6,25 @@ import selectExpenses from '../selectors/expenses';
 // HOC
 // The 'key' value in ExpenseListItem component is set as an attribute
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-            props.expenses.map((expense) => {
-              return <ExpenseListItem key={expense.id} {...expense}/>
-            })
-      )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-others">Expense</div>
+      <div className="show-for-others">Amount</div>
+    </div>   
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span> No expenses </span>
+          </div>
+        ) : (
+              props.expenses.map((expense) => {
+                return <ExpenseListItem key={expense.id} {...expense}/>
+              })
+        )
+      }
+    </div>
   </div>
 );
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 import moment from 'moment'; // Timestamp format change
 import numeral from 'numeral'; // Expense format change
 
-// Destructuring the object to 'dispatch', 'id', 'description', 'amount', 'createAt'
-// The component is injected to the 'dispatch' function when we use connect() hoc to wrap it up
+/* Destructuring the object to 'dispatch', 'id', 'description', 'amount', 'createAt'
+   The component is injected to the 'dispatch' function when we use connect() hoc to wrap it up
+   Import 'Link' component to redirect edit page */
+
 const ExpenseListItem = ({ id, description, amount, createAt }) => (
-  <div>
-    {/* Import 'Link' component to redirect edit page */}
-    <Link to={`/edit/${id}`}>
-       <h2> { description } </h2> 
-    </Link>
-    <p> 
+  <Link className="list-item" to={`/edit/${id}`}>
+    <div>
+      <h2 className="list-item__title"> { description } </h2>    
+      <span className="list-item__subtitle"> {moment(createAt).format('MMMM Do, YYYY (ddd)')} </span>
+    </div>
+    <h2 className="list-item__data"> 
       {numeral(amount / 100).format('$0,0.00')} 
-      - 
-      {moment(createAt).format('MMMM Do, YYYY (ddd)')} 
-    </p>
-  </div>
+    </h2> 
+  </Link>
 );
 
 export default ExpenseListItem; 
